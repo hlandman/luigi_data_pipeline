71ceac851d1edc6c7cb5fe7a6c86d95af3166f95
Hillel Landman <hlandman@gmail.com>
2019-04-29 20:03:27 -0400

    feat: implemented visualizer

--- src/final_project/tasks.py
+++ src/final_project/tasks.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import dask.dataframe as dd
+import matplotlib.pyplot as plt
 
 from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
@@ -22,7 +23,7 @@ class GetBadData(ExternalTask):
     DATA_ROOT = 'data\\unclean\\'
 
     # Unclean file's local name as luigi parameter
-    filename = Parameter('small_data_duped.csv')
+    filename = Parameter('winequality-red.csv')
 
     # Specify if "S3" or "Local"
     source_type = Parameter(default="Local")
@@ -86,7 +87,7 @@ class DataCleaner(Task):
         dummy_col: string "none" or name of dummy variable column
     """
 
-    CLEAN_PATH = os.path.join('data', 'cleaned')
+    CLEAN_PATH = os.path.join('data', 'cleaned\\')
     output_type = GetBadData().output_type
     filename = Parameter(GetBadData().filename)
     has_column_names = BoolParameter(default=True)
@@ -95,18 +96,12 @@ class DataCleaner(Task):
     source_type = Parameter(default="Local")
 
     # Parse Dates - False if no date column, column name if there is
+    date_data = BoolParameter(default=False)
     date_column = Parameter(default="date")
 
     # Drop "rows," "columns," "both" or "none"
-    drop_nan = BoolParameter(default="none")
+    drop_nan = Parameter(default="none")
     na_filler = Parameter(default=' ')
-
-    # Encode Categorical Columns - "none" or column name
-    # Multiple columns??
-    category_col = Parameter(default="category")
-
-    # Dummy Columns - "none" or column name
-    dummy_col = Parameter(default='dummy')
 
     def requires(self):
         if self.source_type == "S3":
@@ -120,7 +115,7 @@ class DataCleaner(Task):
         # Returns Target type based on Dask or Pandas
         if self.output_type == "pandas" or "dask":
             # Returns Target
-            return LocalTarget(path=self.CLEAN_PATH)
+            return LocalTarget(path=str(self.CLEAN_PATH) + str(self.filename))
         else:
             raise NotImplementedError
 
@@ -129,9 +124,13 @@ class DataCleaner(Task):
             # Deal with nonstandard missing values
             missing_values = ["n/a", "na", "--"]
 
-            # Read in the Target
-            df = self.input().read_dask(parse_dates=[self.date_column], na_values=missing_values,
-                                        encoding='unicode_escape')
+            if self.date_data:
+                # Read in the Target
+                df = self.input().read_dask(parse_dates=[self.date_column], na_values=missing_values,
+                                            encoding='unicode_escape')
+            else:
+                # Read in the Target
+                df = self.input().read_dask(na_values=missing_values, encoding='unicode_escape')
 
             df = df.compute()
 
@@ -146,17 +145,6 @@ class DataCleaner(Task):
             else:
                 df.fillna(self.na_filler)
 
-            # Encode labels
-            if self.category_col != "none":
-                l_encoder = preprocessing.LabelEncoder()
-                df[self.category_col] = l_encoder.fit_transform(df[self.category_col])
-
-            # Dummy Variables
-            if self.dummy_col != "none":
-                # hot_encoder = OneHotEncoder(handle_unknown='ignore')
-
-                df[self.dummy_col] = pd.get_dummies(df[self.dummy_col])
-
             print(df.head())
 
             # Output to CSV file in "Cleaned" folder
@@ -165,20 +153,103 @@ class DataCleaner(Task):
                 os.mkdir(outdir)
             df.to_csv(os.path.join(outdir, str(GetBadData().filename)))
 
+            # self.output().to_csv(df, compression='gzip')
+
         else:
             raise NotImplementedError
 
 
-class Visualize(Task):
-    """Visualizes Data - Customizably"""
-    VISUAL_PATH = os.path.join('data', 'visualized')
+class DataEncoder(Task):
+    """Encodes variables"""
+    ENCODED_PATH = os.path.join('data', 'encoded\\')
+
+    filename = Parameter(GetBadData().filename)
+
+    # Encode Categorical Columns - "none" or column name
+    # Multiple columns??
+    category_col = Parameter("quality")
+
+    # Dummy Columns - "none" or column name
+    dummy_col = Parameter("good")
 
     def requires(self):
         return DataCleaner()
 
     def output(self):
-        # Returns Local Target
-        return LocalTarget(path=self.VISUAL_PATH)
+        # Returns CSV Target
+        return LocalTarget(path=self.ENCODED_PATH + str(self.filename))
 
     def run(self):
+        df = pd.read_csv(self.input().open('r'))
 
+        # Encode labels
+        if self.category_col != "none":
+            l_encoder = preprocessing.LabelEncoder()
+            df[self.category_col] = l_encoder.fit_transform(df[self.category_col])
+
+        # Dummy Variables
+        if self.dummy_col != "none":
+            # hot_encoder = OneHotEncoder(handle_unknown='ignore')
+
+            df[self.dummy_col] = pd.get_dummies(df[self.dummy_col])
+
+        print(df.head())
+
+        # Output to CSV file in "Cleaned" folder
+        outdir = self.ENCODED_PATH
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        df.to_csv(os.path.join(outdir, str(self.filename)))
+
+
+class DataVisualizer(Task):
+    """Visualizes Data - Customizably"""
+    VISUAL_PATH = os.path.join('data', 'visualized\\')
+
+    figure_name = Parameter('figure.pdf')
+
+    # Do you need Encoding?
+    encoder = BoolParameter(default=True)
+
+    # Specify what type of chart - "scatter," "line," "hist," "bar"
+    chart_type = Parameter(default="scatter")
+    chart_title = Parameter(default="My Chart")
+
+    # Choose column name for bar chart
+    bar_col = Parameter(default="none")
+
+    # Pass in column name for x and y axes
+    x_axis = Parameter(default="sulphates")
+    y_axis = Parameter(default="quality")
+
+    def requires(self):
+        if self.encoder:
+            return DataEncoder()
+        else:
+            return DataCleaner()
+
+    def output(self):
+        # Returns Local Target
+        return LocalTarget(path=self.VISUAL_PATH + str(self.figure_name))
+
+    def run(self):
+        df = pd.read_csv(self.input().open('r'))
+
+        if self.chart_type == "scatter":
+            chart = df.plot.scatter(x=self.x_axis, y=self.y_axis, title=self.chart_title)
+        elif self.chart_type == "line":
+            chart = df.plot.line(title=self.chart_title)
+        elif self.chart_type == "hist":
+            chart = df.plot.hist(subplots=True, layout=(2, 2), figsize=(10, 10), bins=20)
+        # elif self.chart_type == "bar":
+        else:
+            chart = df[self.bar_col].value_counts().sort_index().plot.bar()
+
+        fig = chart.get_figure()
+
+        # Output to CSV file in "Cleaned" folder
+        outdir = self.VISUAL_PATH
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        fig.savefig(os.path.join(outdir, str(self.figure_name)))
+        # df.to_csv(os.path.join(outdir, str(self.figure_name)))
